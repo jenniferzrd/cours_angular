2d49feb380a94966ce60e1185016f7a14e5937cc
jenniferzrd <jennifer.zarid@gmail.com>
2017-10-13 09:41:48 +0200

    dossier cours exercices et tests

---
+++ my-dream-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CommunicationModule } from './communication/communication.module';
+import { AppPremier } from './app.premier';
+import { AppComponent } from './app.component';
+import { TestViaCommandeComponent } from './test-via-commande/test-via-commande.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { ShopComponent } from './shop/shop.component';
+
+
+@NgModule({
+  declarations: [
+   AppComponent
+   // AppPremier,
+  // TestViaCommandeComponent
+/*  ShopComponent*/],
+  imports: [
+    BrowserModule,
+    CommunicationModule
+    //BrowserAnimationsModule,
+    //MatButtonModule,
+    //MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [
+  AppComponent
+/*  AppPremier*/
+/*  ShopComponent*/
+  ],
+  // exports: [
+  //   MatButtonModule,
+  //   MatCheckboxModule
+  // ]
+})
+export class AppModule { }
